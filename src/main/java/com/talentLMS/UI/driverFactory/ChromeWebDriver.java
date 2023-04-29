e56534f658286978c312c48046a1df1ea780fd88
Kayrat <zhaparbekuulu@gmail.com>
2023-04-29 12:02:28 +0600

    sandbox

--- src/main/java/com/talentLMS/UI/driverFactory/ChromeWebDriver.java
+++ src/main/java/com/talentLMS/UI/driverFactory/ChromeWebDriver.java
@@ -2,6 +2,7 @@ package com.talentLMS.UI.driverFactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -15,6 +16,10 @@ public class ChromeWebDriver {
 
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.addArguments("--start-maximized");
+        options.addArguments("--no-sandbox");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
 
